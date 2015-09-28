56dc97c30f4bbe6f53c5e5ab90aca7c146b0b080
alokp <alokpra@gmail.com>
2015-09-28 16:32:31 -0700

    ip address location fixed

--- app/controllers/food_trucks_controller.rb
+++ app/controllers/food_trucks_controller.rb
@@ -1,8 +1,11 @@
 class FoodTrucksController < ApplicationController
 
   def index
-
-    @truckData = FoodTruck.data(params[:address])
+    if params[:address]
+      @truckData = FoodTruck.data(params[:address])
+    else
+      @truckData = FoodTruck.data(request.remote_ip)
+    end
     render json: @truckData.to_json(include: :locations), status: 200
 
   end
