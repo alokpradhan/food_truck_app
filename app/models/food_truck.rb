df9ed65e83d76298b12007469ed572c198c9cd91
Joseph Lozano <lozanojoseph@gmail.com>
2015-09-29 11:31:41 -0700

    comments

--- app/models/food_truck.rb
+++ app/models/food_truck.rb
@@ -5,13 +5,15 @@ class FoodTruck < ActiveRecord::Base
   has_many :locations, through: :operations
 
   def self.data(location)
-
     find = FoodTruck.new
 
+     # check to see if an array of coords was passed
     if (location[0] == '[')
       geo_location = JSON.parse(location)
+    # check to see if a street adress was passed
     elsif location[-1] =='A'
       geo_location = Geocoder.coordinates(location)
+    # else an ip address was passed
     else
       response = HTTParty.get('http://ip-api.com/json/'+location)
       geo_location = [response['lat'], response['lon']]
@@ -31,7 +33,7 @@ class FoodTruck < ActiveRecord::Base
     locations.each do |truckLocation|
       food_trucks_result.push(truckLocation.food_trucks)
     end
-
+    # recursive function call to increase search radius until 26 trucks are found
     if food_trucks_result.length < 26 && radius < 0.2
       trucks_near_locations(geo_location, radius + 0.002, food_trucks_result)
     end
